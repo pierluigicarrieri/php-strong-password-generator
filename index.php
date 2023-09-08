a1c51d3b6af48f216288b32a3c871070f3a3963b
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-09-08 17:35:18 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <title>php-strong-password-generator</title>
+</head>
+
+<body>
+
+    <div class="container">
+
+        <h1 class="mt-5 text-center text-danger">PHP PASSWORD GENERATOR</h1>
+
+        <div class="w-25 mt-5 mb-3 m-auto">
+            <label for="pwLength" class="form-label mb-4">Please enter the desired length of password</label>
+            <input type="email" class="form-control" id="pwLength" placeholder="Number of characters">
+        </div>
+
+        <h3 class="mt-5 mb-5 text-center text-danger">Your password is:</h3>
+
+        <p class="w-75 m-auto text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam deleniti libero eos maiores, asperiores quo corrupti ab. Voluptatibus sunt maxime veniam omnis. Provident repudiandae assumenda enim odio asperiores earum adipisci?</p>
+
+    </div>
+
+</body>
+
+</html>
